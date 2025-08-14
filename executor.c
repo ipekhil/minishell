b74d928f6ccbc0d045ff6ac11a40bd9633d2f092
sude <sudetaylan68@gmail.com>
2025-08-14 22:37:27 +0300

    handle error fd < 0

--- executor.c
+++ executor.c
@@ -6,7 +6,7 @@
 /*   By: sude <sude@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 19:40:29 by sude              #+#    #+#             */
-/*   Updated: 2025/08/14 22:25:37 by sude             ###   ########.fr       */
+/*   Updated: 2025/08/14 22:37:02 by sude             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,7 @@ void	apply_redirections(t_data *data, t_redirection *redir)
 		{
 			fd = open(redir->filename, O_RDONLY);
 			if (fd < 0)
-			{
 				handle_error_and_exit(data, redir->filename, ": No such file or directory\n", 1);
-				exit(1);
-			}
 			dup2(fd, STDIN_FILENO);
 			close(fd);
 		}
@@ -43,10 +40,7 @@ void	apply_redirections(t_data *data, t_redirection *redir)
 		{
 			fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			if (fd < 0)
-			{
-				perror("open");
-				exit(1);
-			}
+				handle_error_and_exit(data, redir->filename, ": No such file or directory\n", 1);
 			dup2(fd, STDOUT_FILENO);
 			close(fd);
 		}
@@ -54,10 +48,7 @@ void	apply_redirections(t_data *data, t_redirection *redir)
 		{
 			fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
 			if (fd < 0)
-			{
-				perror("open");
-				exit(1);
-			}
+				handle_error_and_exit(data, redir->filename, ": No such file or directory\n", 1);
 			dup2(fd, STDOUT_FILENO);
 			close(fd);
 		}
@@ -65,16 +56,10 @@ void	apply_redirections(t_data *data, t_redirection *redir)
 		{
 			fd = open(redir->filename, O_RDONLY);
 			if (fd < 0)
-			{
-				printf("minishell: %s: No such file or directory\n", redir->filename);
-				perror("open");
-				exit(1);
-			}
+				handle_error_and_exit(data, redir->filename, ":No such file or directory\n", 1);
 			dup2(fd, STDIN_FILENO);
 			close(fd);
 		}
-		// if (!redir->next && access("heredoc_tmp", F_OK) == 0)
-		//  	unlink("heredoc_tmp");
 		redir = redir->next;
 	}
 }
